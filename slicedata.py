9f4b6bb876ffed6138274860168dae4e079f4cb8
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-04-02 20:41:12 +0000

    sliceexample

---
+++ slicedata.py
@@ -0,0 +1,13 @@
+text = " Python Programming Language"
+
+sliceObj = slice(5)
+
+print(text[sliceObj])
+
+sliceObj = slice(6,12)
+
+print(text[sliceObj])
+
+sliceObj = slice(6,25,5)
+
+print(text[sliceObj])
