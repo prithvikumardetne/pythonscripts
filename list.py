87fda1aff656be9a60bf8b21d82eb1398582f96e
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-04-02 20:18:15 +0000

    list example has been added

---
+++ list.py
@@ -0,0 +1,13 @@
+cars=["Toyota","Honda","Kia","Hyundai"]
+
+print(cars)
+
+cars.insert(1,"Nissan")
+
+print("List after insertion:")
+print(cars)
+
+cars.remove("Kia")
+
+print("List After deletion")
+print(cars)
