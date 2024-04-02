d43c1f59d4baec79d4d1dac14d39509f98ad1e06
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-04-02 21:18:38 +0000

    functionexample

---
+++ function.py
@@ -0,0 +1,12 @@
+def addition(number1,number2):
+    result = number1 + number2
+    print("Addition Result:",result)
+
+    addition(4, 5)
+
+def area(radius):
+    result = 3.14 * radius * radius
+    return result
+
+
+print(" Area of the circle is",area(2))
