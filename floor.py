23307db38659d49bf6793c82f3bb0652df92e343
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-04-05 15:27:57 +0000

    floor and ceil functions

---
+++ floor.py
@@ -0,0 +1,9 @@
+#The floor() function is used for getting the floor integer of "X", which mean the largest integer value, which is not greater than "X", basically the nearest round-down number of it.
+
+import math as M
+
+print ("the floor value of math.floor(-54.21) is: ", M.floor(-54.21))
+
+print ("the floor value of math.floor(432.25) is: ", M.floor(432.25))
+
+print ("the ceil value of math.floor(432.25) is: ", M.ceil(432.25))
