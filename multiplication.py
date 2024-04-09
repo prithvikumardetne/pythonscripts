b7275c17493d540f8c99f053c0c110f99a38bfce
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-04-09 15:38:19 +0000

    multiplication table

---
+++ multiplication.py
@@ -0,0 +1,7 @@
+def print_multiplication_table(number):
+    print("Multiplication table of {number}:")
+    for i in range(1,11):
+        print(f"{number}x{i} = {result}")
+
+num = int(input("Enter a number:"))
+print_multiplication_table(num)
--- random.py
+++ random.py
@@ -1,5 +1,5 @@
 import random
 
-random_number = random(1, 2000)
+random_number = rand.int(1, 2000)
 
 print("Random Number between 1 and 2000 is: ", random_number)
