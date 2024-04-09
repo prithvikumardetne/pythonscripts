f52930e6288a63b6c166e8be4b1bf7722facb2d4
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-04-09 15:42:29 +0000

    multplication table udpated one

--- multiplication.py
+++ multiplication.py
@@ -1,7 +1,8 @@
 def print_multiplication_table(number):
     print("Multiplication table of {number}:")
     for i in range(1,11):
-        print(f"{number}x{i} = {result}")
+        result = number*i
+        print(f"{number}x{i}={result}")
 
 num = int(input("Enter a number:"))
 print_multiplication_table(num)
