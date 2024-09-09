9c16b5e25d69e3ee09eda45fdd3222c808c94375
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-09-09 04:08:14 +0000

    Addind Split.py

---
+++ split.py
@@ -0,0 +1,22 @@
+text = 'Mary had a little lamb'
+text1 = text.split(" ")
+
+print(text1[0])
+
+text2 = " and " .join(text1)
+
+print(text2)
+
+name = "prithvi kuamr detne"
+
+name1 =name.capitalize()
+
+print(name1)
+
+name2 = name.upper()
+
+print(name2)
+
+name3 = name.swapcase()
+
+print(name3)
