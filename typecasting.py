d3b899194db1d41d9a57db83333c049e2ce30a76
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-09-27 19:57:58 +0000

    Typecasting code

---
+++ typecasting.py
@@ -0,0 +1,7 @@
+current_salary = input("What is the current salary")
+
+hike = input("What is the hike")
+
+new_salary = int(current_salary) + ( int(current_salary) * int(hike))
+
+print(new_salary)
