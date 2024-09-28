18cd84cef18f31bbc26fe2e8029534219b18e184
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-09-28 03:50:04 +0000

    Adding some more programs

--- hellodataengineer1.py
+++ hellodataengineer1.py
@@ -1,3 +1,5 @@
+import math
+
 name="prithvi"
 
 print(type(name))
@@ -8,6 +10,7 @@ print(type(age))
 
 amount=100.52
 
+
 print(type(amount))
 
 is_male=True
@@ -17,3 +20,15 @@ print(amount + 50)
 
 print (int(age) + (int(age) * .1))
 
+d = 115.46
+
+print(math.ceil(d))
+
+marks = int(input("Enter the Marks: "))
+
+if marks > 35:
+    print("Pass")
+elif marks > 35 & marks < 70:
+    print ("First Grade")
+else:
+    print("failed")
---
+++ sample-program.py
@@ -0,0 +1,17 @@
+first_name = "Prithvi Kumar"
+
+last_name = "Detne"
+
+
+print(first_name  + last_name)
+
+numbers = ["5","8","7"]
+
+numbers.sort()
+
+print ("Sorted numbers are {numbers}")
+
+print( 4 + 6 * 9 % 5)
+
+print( 3 ** 2 ** 2)
+
