cb057b3b96807e64ecba3e9f7eba6d5d7e4fe409
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-09-28 21:55:42 +0000

    playing around with string

--- indexing.py
+++ indexing.py
@@ -7,3 +7,11 @@ print (name[:index])#printing string until first place
 print(name[0:9:2]) # printing string alternatively
 
 print(name[::-1]) # reversing a string
+
+print(name.find("K")) # will print the indexing of first occurrence of K
+
+print( name.find("S")) # Printing -1 since it didnt find indexing of the string
+
+print(name.endswith("r")) #endswith is a method which will help in finding last word or last index
+
+print(name.replace("Prithvi","DPK")) # replace method
