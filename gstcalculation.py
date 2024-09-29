d1c3808bfe5bbd6e0b73de5e1c5e3b287e52dbda
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-09-29 18:35:25 +0000

    one line code for GST Calculation

--- gstcalculation.py
+++ gstcalculation.py
@@ -1,10 +1,14 @@
 bill_amount = [100,200,300,400,500,600]
 
-bill_after_gst = []
+'''bill_after_gst = []
 
 for x in bill_amount:
     bill_after_gst.append(x+x*.18)
 
+
+print(bill_after_gst)'''
+
+bill_after_gst = [x+x*.18 for x in bill_amount]
 
 print(bill_after_gst)
 
