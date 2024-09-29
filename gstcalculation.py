6b5fb4e001194c0a98f464493cf9efa35ca694c7
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-09-29 19:17:13 +0000

    list comprehension code

--- gstcalculation.py
+++ gstcalculation.py
@@ -8,7 +8,16 @@ for x in bill_amount:
 
 print(bill_after_gst)'''
 
-bill_after_gst = [x+x*.18 for x in bill_amount]
+'''bill_after_gst = [x+x*.18 for x in bill_amount]
 
-print(bill_after_gst)
+print(bill_after_gst)'''
 
+bill_amount = [(100,5),(200,8),(300,6),(400,5),(500,5),(600,8)]
+
+bill_amount_with_gst = [x[0] + x[0] * x[1]/100  for x in bill_amount]
+
+"""bill_amount_with_gst = []
+for x in bill_amount:
+    bill_amount_with_gst.append(x[0] + x[0] * x[1]/100)"""
+
+print(bill_amount_with_gst)
