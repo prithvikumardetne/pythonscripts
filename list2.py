0e289316241dfab8b4eba0a228075f67ecbc25cd
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-09-29 01:11:37 +0000

    Changing string to list

---
+++ list2.py
@@ -0,0 +1,9 @@
+#names_list = ["Prithvi","Kumar","Detne"] - It is mutable
+#names_tuple = ("Prithvi","Kumar","Detne") - It is immutable
+#range =(0-9)
+
+name = "Prithvi Kumar Detne "
+
+ordered_name = name.split(" ")
+
+print(ordered_name)
