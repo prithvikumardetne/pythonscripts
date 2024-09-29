665aa9a8f203aec20ac12364bf3aa2ec489f1cf7
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-09-29 02:33:45 +0000

    Playing with Tuple and List

--- list2.py
+++ list2.py
@@ -17,3 +17,17 @@ names = ["A","B","C","D"]
 names[2]="E"
 
 print(names)
+
+names_tuple = ("A","B","C","D")
+
+names_list = list(names_tuple)
+
+print(names_list)
+
+names_list.insert(1,'E')
+
+print(names_list)
+
+names_tuple = tuple(names_list)
+
+print(names_tuple)
