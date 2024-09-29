4dc2adce038776e53c2b63f8ee7ede3ff7c65dd7
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-09-29 23:50:08 +0000

    Adding only one line of code

--- listcomprehension.py
+++ listcomprehension.py
@@ -2,7 +2,9 @@
 
 list1 = []
 
-for x in range(1,4):
-    list1.append([x,x ** 2,x ** 3])
+list1 = [[x,x ** 2,x ** 3] for x in range(1,4)] 
+
+'''for x in range(1,4):
+    list1.append([x,x ** 2,x ** 3])'''
 
 print(list1)
