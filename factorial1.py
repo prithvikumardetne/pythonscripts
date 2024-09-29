27845b8e6af17b03933f3c98d9fcd57749000314
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-09-29 16:59:27 +0000

    GST Calculation Program

---
+++ factorial1.py
@@ -0,0 +1,9 @@
+factorial_number = 5
+
+for i in factorial_number:
+    if i == 0:
+    factorial = factorial_number * (factorial_number-1)
+    else:
+
+
+print(factorial)
---
+++ gstcalculation.py
@@ -0,0 +1,10 @@
+bill_amount = ["100","200","300","400","500","600"]
+
+bill_after_gst = []
+
+for x in bill_amount:
+    bill_after_gst.append(x+x*.18)
+
+
+print(bill_after_gst)
+
