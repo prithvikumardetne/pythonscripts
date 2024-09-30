235fca1533a52a30183521fc6c98d7e3947b4fa0
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-09-30 22:51:32 +0000

    Reverse sorting code

--- listing_range of numbers.py
+++ listing_range of numbers.py
@@ -2,3 +2,20 @@ numbers = list(range(1,101))
 
 
 print(numbers)
+
+
+#ascending order
+
+numbers = [1,8,4,9,7,3]
+
+numbers.sort()
+
+print(numbers)
+
+# descending order 
+
+numbers = [1,8,4,9,7,3]
+
+numbers.sort(reverse=True)
+
+print(numbers)
