cff32c13f71c1588b8fbe618ce51a3aa9f9fc264
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-09-30 01:58:19 +0000

    words in sorted order

---
+++ wordssortorder.py
@@ -0,0 +1,15 @@
+
+example = "Hello this Is an Example With cased letters"
+
+words = [i.lower() for i in example.split()] 
+
+print(words)
+
+words.sort
+
+print("The sorted words are: ")
+for word in words:
+    print(word)
+
+    
+        
