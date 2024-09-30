48a04ca5d17c035e04ff45311c5f02eb3c90fe6d
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-09-30 00:03:01 +0000

    Adding Flattened List Code

--- listcomprehension.py
+++ listcomprehension.py
@@ -8,3 +8,10 @@ list1 = [[x,x ** 2,x ** 3] for x in range(1,4)]
     list1.append([x,x ** 2,x ** 3])'''
 
 print(list1)
+
+flattened_lists = []
+
+for sublists in list1:
+    for item in sublists:
+        flattened_lists.append(item)
+print(flattened_lists)
