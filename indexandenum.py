4331fb03b5423ed80d539f6f42cdc126b0d5a3da
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-09-30 23:25:41 +0000

    enumerate in python

---
+++ indexandenum.py
@@ -0,0 +1,4 @@
+list1 = ["Semicolons", "are", "one", "of", "the", "most", "misunderstood", "and", "underused", "punctuation", "marks"]
+
+for index,element in enumerate(list1):
+    print(f'Index: {index}, Element: {element}')
