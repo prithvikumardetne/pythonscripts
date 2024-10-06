451a2b31f8f900ad1900dc62d0cac4dc06f0ec48
Prithvi Kumar Detne <38846173+prithvikumardetne@users.noreply.github.com>
2024-10-06 16:49:23 +0000

    Adding programs using list methods

--- mostfrequest_lessfrequent.py
+++ mostfrequest_lessfrequent.py
@@ -1,5 +1,7 @@
 data = [3,3,4,4,4,5,5,2]
 
+data_extend = [8,9,7,6]
+
 #data_set = set(data)
 
 #print(data_set)
@@ -11,3 +13,41 @@ print(most_frequent)
 less_frequent = min(set(data),key=data.count)
 
 print(less_frequent)
+
+data.sort(reverse=True)
+
+print(data)
+
+data.sort(reverse=False)
+
+print(data)
+
+data.append(6)
+
+print(data)
+
+print(data.count(4))
+
+data.extend(data_extend)
+
+print(data)
+
+data_index = data.index(9)
+
+print(data_index)
+
+data_pop = data.pop()
+
+print(data)
+
+data.remove(9)
+
+print(data)
+
+del data[2:4]
+
+print(data)
+
+data.insert(5,7)
+
+print(data)
